c58ea017a7e0c6e2924f5b98811a5b81035da81a
Mitia Jessica <mitiah.jessica@gmail.com>
2026-04-13 14:32:40 +0300

    add firebase-configjs

---
+++ assets/JS/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBumrtbQIfK3d3e6OExmPRS2zCmrvgsO3E",
+  authDomain: "type-rush-63bba.firebaseapp.com",
+  projectId: "type-rush-63bba",
+  storageBucket: "type-rush-63bba.firebasestorage.app",
+  messagingSenderId: "139699771228",
+  appId: "1:139699771228:web:f1d167ddd5602526949399",
+  measurementId: "G-C3ECTMT0ZW"
+};
+
+// Initialize Firebase using compat syntax
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
